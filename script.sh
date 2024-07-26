8bd65466e834201e5ff4bc87216e173b8a5dea7c
thomasalbertoDev <thomasalberto456@gmail.com>
2024-07-26 10:05:37 +0700

    first commit

---
+++ script.sh
@@ -0,0 +1,5 @@
+# Fungsi ini digunakan untuk melakukan build image dari Dockerfile yang ada di folder 1.from
+docker build -t xriot45/from 1.from
+
+# Fungsi ini digunakan untuk menampilkan semua image yang ada
+docker image ls
