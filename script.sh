3735bdad65062a90af7fd96fcd88c37f1d9529b2
thomasalbertoDev <thomasalberto456@gmail.com>
2024-07-26 11:18:57 +0700

    Belajar Dockerfile

--- script.sh
+++ script.sh
@@ -34,5 +34,17 @@ docker container start add
 # Perintah ini digunakan untuk melihat log dari container yang telah dijalankan
 docker container logs add
 
+# 6. Fungsi ini digunakan untuk melakukan build image dari Dockerfile yang ada di folder 6.copy (COPY INSTRUCTION)
+docker build -t xriot45/copy 6.copy
+
+# Perintah ini digunakan untuk membuat container dari image xriot45/copy
+docker container create --name copy xriot45/copy
+
+# Perintah ini digunakan untuk menjalankan container yang telah dibuat sebelumnya
+docker container start copy
+
+# Perintah ini digunakan untuk melihat log dari container yang telah dijalankan
+docker container logs copy
+
 # Fungsi ini digunakan untuk menampilkan semua image yang ada
 docker image ls
