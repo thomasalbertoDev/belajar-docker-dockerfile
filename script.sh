83c5b1c2157dad2cb4b994802df8144dad3bc764
thomasalbertoDev <thomasalberto456@gmail.com>
2024-07-26 10:34:36 +0700

    belajar perintah command

--- script.sh
+++ script.sh
@@ -1,8 +1,20 @@
-# 1. Fungsi ini digunakan untuk melakukan build image dari Dockerfile yang ada di folder 1.from
+# 1. Fungsi ini digunakan untuk melakukan build image dari Dockerfile yang ada di folder 1.from (FROM INSTRUCTION)
 docker build -t xriot45/from 1.from
 
-# 2. Fungsi ini digunakan untuk melakukan build image dari Dockerfile yang ada di folder 2.run
+# 2. Fungsi ini digunakan untuk melakukan build image dari Dockerfile yang ada di folder 2.run (RUN INSTRUCTION)
 docker build -t xriot45/run 2.run
+
+# 3. Fungsi ini digunakan untuk melakukan build image dari Dockerfile yang ada di folder 3.command (COMMAND / CMD INSTRUCTION)
+docker build -t xriot45/command 3.command
+
+# Perintah ini digunakan untuk membuat container dari image xriot45/command
+docker container create --name command xriot45/command
+
+# Perintah ini digunakan untuk menjalankan container yang telah dibuat sebelumnya
+docker container start command
+
+# Perintah ini digunakan untuk melihat log dari container yang telah dijalankan
+docker container logs command
 
 # Fungsi ini digunakan untuk menampilkan semua image yang ada
 docker image ls
