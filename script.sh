8dfd78fe9499f6e9dde494d4bda6c9ee8b2d9154
thomasalbertoDev <thomasalberto456@gmail.com>
2024-07-26 10:25:51 +0700

    belajar perintah run

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
-# Fungsi ini digunakan untuk melakukan build image dari Dockerfile yang ada di folder 1.from
+# 1. Fungsi ini digunakan untuk melakukan build image dari Dockerfile yang ada di folder 1.from
 docker build -t xriot45/from 1.from
+
+# 2. Fungsi ini digunakan untuk melakukan build image dari Dockerfile yang ada di folder 2.run
+docker build -t xriot45/run 2.run
 
 # Fungsi ini digunakan untuk menampilkan semua image yang ada
 docker image ls
